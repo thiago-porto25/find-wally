10439fd709d20f9e9cf64b5051b9db3addf5a828
thiago-porto25 <thiago_porto25@hotmail.com>
2021-08-27 10:25:30 -0300

    add picture for header

---
+++ src/pages/Choose.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Choose({ levels }) {
+  return (
+    <div className="choose-container">
+      <section className="cards-section">{}</section>
+      <section className="button-section">
+        <button>Leaderboard</button>
+      </section>
+    </div>
+  )
+}
