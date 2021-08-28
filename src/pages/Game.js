9115c01ae7fb5615f12e598a605d4f91f6b47ff7
thiago-porto25 <thiago_porto25@hotmail.com>
2021-08-28 11:55:24 -0300

    work on game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,15 +1,97 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import { findPercentage, findCoordinate } from '../helpers'
 
-export default function Game() {
+export default function Game({
+  selectedLevel,
+  setSelectedLevel,
+  setIsGameRunning,
+  setIsChoosingLevel,
+}) {
+  const image = useRef(null)
+  const [imageIsClicked, setImageIsClicked] = useState(false)
+  const [currentXY, setCurrentXY] = useState(null)
+
+  const handleClick = (e) => {
+    setImageIsClicked((prev) => !prev)
+    trackUserClick(e)
+  }
+
   const trackUserClick = (e) => {
     var rect = e.target.getBoundingClientRect()
     const x = e.clientX - Math.round(rect.left)
     const y = e.clientY - Math.round(rect.top)
-    const resX = findPercentage(x, e.target.width)
-    const resY = findPercentage(y, e.target.height)
-    console.log(resX + ',' + resY)
+    setCurrentXY({ x, y })
   }
 
-  return <div className="game-container">Game</div>
+  const handleSelection = (character) => {
+    const dataX = findCoordinate(
+      parseInt(selectedLevel[character].x, 10),
+      image.current.width
+    )
+    const dataY = findCoordinate(
+      parseInt(selectedLevel[character].y, 10),
+      image.current.height
+    )
+    const result =
+      currentXY.x > dataX - 20 &&
+      currentXY.x < dataX + 20 &&
+      currentXY.y > dataY - 20 &&
+      currentXY.y < dataY + 20
+
+    console.log(currentXY.x + ',' + currentXY.y)
+    console.log(dataX + ',' + dataY)
+    console.log(result)
+
+    setImageIsClicked(false)
+  }
+
+  const listStyle = {
+    left: currentXY && currentXY.x + 20,
+    top: currentXY && currentXY.y,
+    zIndex: 2,
+  }
+
+  const squareStyle = {
+    transform: 'translate(-10px, -10px)',
+    left: currentXY && currentXY.x,
+    top: currentXY && currentXY.y,
+  }
+
+  return (
+    <div className="game-container">
+      <div className="game-inner">
+        <img
+          ref={image}
+          onClick={handleClick}
+          src={selectedLevel.url}
+          alt="Game Board"
+        />
+        {imageIsClicked && (
+          <div style={listStyle} className="chars-list-container">
+            <div
+              onClick={() => handleSelection('wally')}
+              className="chars-list-item"
+            >
+              <p>Wally</p>
+            </div>
+            <div
+              onClick={() => handleSelection('wilma')}
+              className="chars-list-item"
+            >
+              <p>Wilma</p>
+            </div>
+            <div
+              onClick={() => handleSelection('wizard')}
+              className="chars-list-item"
+            >
+              <p>Wizard</p>
+            </div>
+          </div>
+        )}
+        {imageIsClicked && (
+          <div style={squareStyle} className="selection-square"></div>
+        )}
+      </div>
+    </div>
+  )
 }
