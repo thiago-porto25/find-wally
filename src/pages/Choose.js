864b6fe4d9667b5b1f8e477d02cc23b42d2dd76f
thiago-porto25 <thiago_porto25@hotmail.com>
2021-08-30 12:15:42 -0300

    rm unused functions

--- src/pages/Choose.js
+++ src/pages/Choose.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import LevelCard from '../components/LevelCard'
 
 export default function Choose({ levels, ...props }) {
