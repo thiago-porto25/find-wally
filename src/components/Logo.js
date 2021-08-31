fb7c0deeda4dcd27e8c29927dfc63890155ccf0f
thiago-porto25 <thiago_porto25@hotmail.com>
2021-08-31 16:37:59 -0300

    rm pointer events in specific cases

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 export default function Logo({ name = null, ...props }) {
+  const style = { pointerEvents: name ? 'none' : 'all' }
   return (
-    <header {...props} className={`${name}`}>
+    <header {...props} style={style} className={`${name}`}>
       <img src="/images/wally.png" alt="Wally" />
       Find <span>Wally</span>
     </header>
