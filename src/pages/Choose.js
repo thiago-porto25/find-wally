1b0bb69cd87a7c923b02344b2bef1e9bde7ffa35
thiago-porto25 <thiago_porto25@hotmail.com>
2021-08-31 22:09:18 -0300

    add logic for loading skel to change size

--- src/pages/Choose.js
+++ src/pages/Choose.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Skeleton from 'react-loading-skeleton'
 import LevelCard from '../components/LevelCard'
 
@@ -7,14 +7,27 @@ export default function Choose({ levels, ...props }) {
     props.setIsChoosingLevel(false)
     props.setIsOnLeaderboard(true)
   }
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth)
+  }, [])
 
   return (
     <div className="choose-container">
       <section className="cards-section">
         {!levels[0] && (
           <>
-            <Skeleton count={1} width={300} height={350} />
-            <Skeleton count={1} width={300} height={350} />
+            <Skeleton
+              count={1}
+              width={windowWidth > 330 ? 300 : 250}
+              height={windowWidth > 330 ? 350 : 380}
+            />
+            <Skeleton
+              count={1}
+              width={windowWidth > 330 ? 300 : 250}
+              height={windowWidth > 330 ? 350 : 380}
+            />
           </>
         )}
         {levels &&
