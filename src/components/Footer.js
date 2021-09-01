71757ce4049d28780f8dd437d12f386571166814
thiago-porto25 <thiago_porto25@hotmail.com>
2021-08-31 21:50:50 -0300

    style game page for mobile devices

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,10 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
+      <p className="mobile-message">
+        For mobiles users: It's advised to play with your device in horizontal
+        position.
+      </p>
       <p>
         Webpage created by{' '}
         <a href="https://github.com/thiago-porto25">Thiago Porto</a>
