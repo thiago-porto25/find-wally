8f19a3b2aa4559b71fda35bd134e58ebc47f6dcd
thiago-porto25 <thiago_porto25@hotmail.com>
2021-08-31 22:08:38 -0300

    change message, only shows in game page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-export default function Footer() {
+export default function Footer({ isGameRunning }) {
   return (
     <footer>
-      <p className="mobile-message">
-        For mobiles users: It's advised to play with your device in horizontal
-        position.
-      </p>
+      {isGameRunning && (
+        <p className="mobile-message">
+          For mobiles users: It's advised to play with your device in horizontal
+          position.
+        </p>
+      )}
       <p>
         Webpage created by{' '}
         <a href="https://github.com/thiago-porto25">Thiago Porto</a>
